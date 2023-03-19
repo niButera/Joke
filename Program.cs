c3e5efd187da5aa750af7605d055d3ead814b6fd
NiButera <nikolasxp683@gmail.com>
2023-03-19 03:02:32 -0300

    Escrevi uma piada muito ruim kkkkkk

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿#pragma warning disable
+Console.WriteLine("Era uma vez um pintinho sem fiofó");
+
+Thread.Sleep(2000); 
+Console.WriteLine("Um dia ele foi peidar e");
+
+Thread.Sleep(1000); Console.Beep(1320, 500); Console.ForegroundColor = ConsoleColor.Red; 
+Console.WriteLine("BOOM!");
+
+Thread.Sleep(1000); Console.ForegroundColor = ConsoleColor.White; 
+Console.WriteLine("explodiu");
